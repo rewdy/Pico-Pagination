127a8f9e54b7b3501d49239b1bd0abea53a050a5
Alexander Donald <adonald@users.noreply.github.com>
2014-07-10 13:48:46 +0100

    Allow for Pagination to be performed on sub page
    Added new boolean config variable - "sub_page". When this is true, the $url variable is set to the "page_indicator" variable.
    
    Example usage:
    1. Create a page called "blog" with a new template that lists the pages with dates (as per usual pagination)
    2. Set "page_indicator" to "blog"
    3. Set "sub_page" to true
    4. Have landing page as main site index, and paginated list of posts under /blog - allows for different template to be used for each, and means the pagination does not have to be on the root of the site.

--- pagination.php
+++ pagination.php
@@ -24,7 +24,8 @@ class Pagination {
 			'page_indicator' => 'page',
 			'output_format'	=> 'links',
 			'flip_links' => false,
-			'filter_date' => true
+			'filter_date' => true,
+			'sub_page' => false
 		);
 	}
 
@@ -45,6 +46,8 @@ class Pagination {
 			$this->config['page_indicator'] = $settings['pagination_page_indicator'];
 		if (isset($settings['pagination_output_format']))
 			$this->config['output_format'] = $settings['pagination_output_format'];
+		if (isset($settings['pagination_sub_page']))
+			$this->config['sub_page'] = $settings['pagination_sub_page'];
 	}
 
 	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
@@ -125,7 +128,11 @@ class Pagination {
 			$page_numbers = explode('/', $url);
 			$page_number = $page_numbers[count($page_numbers)-1];
 			$this->page_number = $page_number;
-			$url = '';
+			if ($this->config['sub_page']) {
+				$url = $this->config['page_indicator'];
+			} else {
+				$url = '';
+			}
 		} else {
 			$this->page_number = 1;
 		}
