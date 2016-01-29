b9ecf089b6ead50ca5ffd8c6f21a62335f7d684d
Olav Schettler <olav@schettler.net>
2016-01-29 17:59:11 +0100

    Made to work with PicoCMS 1.0

--- pagination.php
+++ pagination.php
@@ -7,7 +7,7 @@
  * @license http://opensource.org/licenses/MIT
  * @version 1.4
  */
-class Pagination {
+class Pagination extends AbstractPicoPlugin {
 	
 	public $config = array();
 	public $offset = 0;
@@ -15,8 +15,10 @@ class Pagination {
 	public $total_pages = 1;
 	public $paged_pages = array();
 
-	public function __construct()
+	public function __construct(Pico $pico)
 	{
+		parent::__construct($pico);
+
 		$this->config = array(
 			'limit' => 5,
 			'next_text' => 'Next &gt;',
@@ -29,7 +31,7 @@ class Pagination {
 		);
 	}
 
-	public function config_loaded(&$settings)
+	public function onConfigLoaded(&$settings)
 	{
 		// Pull config options for site config
 		if (isset($settings['pagination_limit']))
@@ -50,7 +52,7 @@ class Pagination {
 			$this->config['sub_page'] = $settings['pagination_sub_page'];
 	}
 
-	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
+	public function onPagesLoaded(&$pages, &$currentPage, &$previousPage, &$nextPage)
 	{
 		// Filter the pages returned based on the pagination options
 		$this->offset = ($this->page_number-1) * $this->config['limit'];
@@ -69,42 +71,40 @@ class Pagination {
 		$this->total_pages = ceil(count($show_pages) / $this->config['limit']);
 		// slice show_pages to the limit
 		$show_pages = array_slice($show_pages, $this->offset, $this->config['limit']);
-		// set set filtered pages to paged_pages
+		// set filtered pages to paged_pages
 		$this->paged_pages = $show_pages;
 	}
 
-	public function before_render(&$twig_vars, &$twig)
+	public function onPageRendering(&$twig, &$twigVariables, &$templateName)
 	{
 		// Set a bunch of view vars
 
-		global $config;
-
 		// send the paged pages in separate var
 		if ($this->paged_pages)
-			$twig_vars['paged_pages'] = $this->paged_pages;
+			$twigVariables['paged_pages'] = $this->paged_pages;
 
 		// set var for page_number
 		if ($this->page_number)
-			$twig_vars['page_number'] = $this->page_number;
+			$twigVariables['page_number'] = $this->page_number;
 
 		// set var for total pages
 		if ($this->total_pages)
-			$twig_vars['total_pages'] = $this->total_pages;
+			$twigVariables['total_pages'] = $this->total_pages;
 
 		// set var for page_indicator
-		$twig_vars['page_indicator'] = $this->config['page_indicator'];
+		$twigVariables['page_indicator'] = $this->config['page_indicator'];
 		
 		// build pagination links
 		// set next and back link vars to empty. links will be added below if they are available.
-		$twig_vars['next_page_link'] = $twig_vars['prev_page_link'] = '';
+		$twigVariables['next_page_link'] = $twigVariables['prev_page_link'] = '';
 		$pagination_parts = array();
 		if ($this->page_number > 1) {
-			$prev_path = $config['base_url'] . '/' . $this->config['page_indicator'] . '/' . ($this->page_number - 1);
-			$pagination_parts['prev_link'] = $twig_vars['prev_page_link'] = '<a href="' . $prev_path . '" id="prev_page_link">' . $this->config['prev_text'] . '</a>';
+			$prev_path = $this->getBaseUrl() . '/' . $this->config['page_indicator'] . '/' . ($this->page_number - 1);
+			$pagination_parts['prev_link'] = $twigVariables['prev_page_link'] = '<a href="' . $prev_path . '" id="prev_page_link">' . $this->config['prev_text'] . '</a>';
 		}
 		if ($this->page_number < $this->total_pages) {
-			$next_path = $config['base_url'] . '/' . $this->config['page_indicator'] . '/' . ($this->page_number + 1);
-			$pagination_parts['next_link'] = $twig_vars['next_page_link'] = '<a href="' . $next_path . '" id="next_page_link">' . $this->config['next_text'] . '</a>';
+			$next_path = $this->getBaseUrl() . '/' . $this->config['page_indicator'] . '/' . ($this->page_number + 1);
+			$pagination_parts['next_link'] = $twigVariables['next_page_link'] = '<a href="' . $next_path . '" id="next_page_link">' . $this->config['next_text'] . '</a>';
 		}
 
 		// reverse order if flip_links is on
@@ -114,16 +114,16 @@ class Pagination {
 
 		// create pagination links output
 		if ($this->config['output_format'] == "list") {
-			$twig_vars['pagination_links'] = '<ul id="pagination"><li>' . implode('</li><li>', array_values($pagination_parts)) . '</li></ul>';
+            $twigVariables['pagination_links'] = '<ul id="pagination"><li>' . implode('</li><li>', array_values($pagination_parts)) . '</li></ul>';
 		} else {
-			$twig_vars['pagination_links'] = implode(' ', array_values($pagination_parts));
+            $twigVariables['pagination_links'] = implode(' ', array_values($pagination_parts));
 		}
 
 		// set page of page var
-		$twig_vars['page_of_page'] = "Page " . $this->page_number . " of " . $this->total_pages . ".";
+        $twigVariables['page_of_page'] = "Page " . $this->page_number . " of " . $this->total_pages . ".";
 	}
 
-	public function request_url(&$url)
+	public function onRequestUrl(&$url)
 	{
 		// checks for page # in URL
 		$pattern = '/' . $this->config['page_indicator'] . '\/[0-9]*$/';
